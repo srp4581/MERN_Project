68daabb3f0294cfe66209cccb61e75c8680e5ba4
Sasha Persaud <srp4581@g.rit.edu>
2020-12-30 08:57:30 -0500

    added /dashboard route to navigation component

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -4,11 +4,13 @@ import { store } from '../store'
 import { ConnectedDashboard } from './Dashboard'
 import { Router, Route } from 'react-router-dom'
 import { history } from '../store/history'
+import { ConnectedNavigation } from './Navigation'
 
 export const Main = () => (
     <Router history={history}>
         <Provider store={store}>
             <div>
+                <ConnectedNavigation/>
                 <Route exact path="/dashboard" render={ () => (<ConnectedDashboard/>) }/>
             </div>
         </Provider>
