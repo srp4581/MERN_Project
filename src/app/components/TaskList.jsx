2341b955e3b2a8ec1cb6c0fe7e8e99f457ce759d
Sasha Persaud <srp4581@g.rit.edu>
2020-12-30 20:57:22 -0500

    added mock saga for requesting new task

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { requestTaskCreation } from '../store/mutations';
 
-export const TaskList = ({tasks, name}) =>(
+export const TaskList = ({tasks, name, id, createNewTask}) =>(
     <div>
         <h3>
             {name}
         </h3>
         <div>
-            {tasks.map(task=>(<div>{task.name}</div>))}
+            {tasks.map(task=>(<div key={task.id}>{task.name}</div>))}
         </div>
+        <button onClick={ () => createNewTask(id) }>Add New</button>
     </div>
 )
 
@@ -21,4 +23,13 @@ const mapStateToProp = (state, ownProps) => {
     }
 };
 
-export const ConnectedTaskList = connect(mapStateToProp)(TaskList);
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        createNewTask(id) {
+            console.log("Creating new task...", id);
+            dispatch(requestTaskCreation(id));
+        }
+    }
+};
+
+export const ConnectedTaskList = connect(mapStateToProp, mapDispatchToProps)(TaskList);
