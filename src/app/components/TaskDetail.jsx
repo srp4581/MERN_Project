dfd35c0ba4efda2eacda8caca0c7fe3d5e6b4f0d
Sasha Persaud <srp4581@g.rit.edu>
2021-01-03 13:04:08 -0500

    added more structure and content to the task detail page

--- src/app/components/TaskDetail.jsx
+++ src/app/components/TaskDetail.jsx
@@ -1,17 +1,52 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'; 
+import { Link } from 'react-router-dom';
 
 const TaskDetail = ({
     id,
     comments,
     task,
-    isComplete
+    isComplete,
+    groups
 }) => (
     <div>
-        Task Detail
+        <div>
+            <input value={task.name}/>
+        </div>
+
+        <div>
+            <button>Complete / Reopen Task</button>
+        </div>
+
+        <div>
+            <select>
+                {groups.map(group=>(
+                    <option key={group.id} value={group.id}>{group.name}</option>
+                ))}
+            </select>
+        </div>
+
+        <div>
+            <Link to="/dashboard">
+                <button>Done</button>
+            </Link>
+        </div>
+        
     </div>
 );
 
-const mapStateToProps = state=>state;
+const mapStateToProps = (state, ownProps)=>{
+    let id = ownProps.match.params.id;
+    let task = state.tasks.find(task=>task.id === id);
+    let groups = state.groups;
 
-export const ConnectedTaskDetail = connect (mapStateToProps)(TaskDetail);
+    return {
+        id,
+        task,
+        groups,
+        isComplete: task.isComplete
+    }
+
+}
+
+export const ConnectedTaskDetail = connect(mapStateToProps)(TaskDetail);
